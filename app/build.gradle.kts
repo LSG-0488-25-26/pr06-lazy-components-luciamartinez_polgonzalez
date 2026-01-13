a97069bdaf63bc5687507d351c39933ff4604ccd
Pol Gonzàlez <pol.gonzalez@gracia.lasalle.cat>
2026-01-14 00:10:31 +0100

    build(deps): afegir dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.1")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
