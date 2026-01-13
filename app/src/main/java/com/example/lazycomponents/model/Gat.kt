5f05fc693151295744c5c1f51f5ffd84d7d546b1
Pol Gonzàlez <pol.gonzalez@gracia.lasalle.cat>
2026-01-14 00:14:14 +0100

    feat(model): Crear DataClass Gat

---
+++ app/src/main/java/com/example/lazycomponents/model/Gat.kt
@@ -0,0 +1,9 @@
+package com.example.lazycomponents.model
+
+data class Gat(
+    val id: String,
+    val nombre: String,
+    val tags: List<String>,
+    val urlImagen: String,
+    val descripcion: String
+)
